c8e681fa155004a09766370a4930d7e36c97acb4
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2025-02-24 17:28:14 +0200

    Create Get file size.cpp

---
+++ Week 2/Get file size.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+size_t getFileSize(const char* fileName)
+{
+	if (!fileName) return 0;
+
+	std::ifstream ifs(fileName);
+	if (!ifs.is_open()) return 0;
+
+	size_t prevPos = ifs.tellg();
+	ifs.seekg(0, std::ios::end);
+	size_t fileSize = ifs.tellg();
+	ifs.seekg(prevPos);
+
+	return fileSize;
+}
+
+int main()
+{
+	std::cout << getFileSize("FileName.cpp");
+
+	return 0;
+}
