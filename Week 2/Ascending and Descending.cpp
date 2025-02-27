b79848a088f19619f2d5a053536d5732a1f97213
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2025-02-27 23:35:25 +0200

    Update Ascending and Descending.cpp

--- Week 2/Ascending and Descending.cpp
+++ Week 2/Ascending and Descending.cpp
@@ -1,42 +1,78 @@
 #include <iostream>
 #include <fstream>
 
-size_t getCountOfNums(std::ifstream& ifs)
-{
-	if (!ifs.is_open()) return 0;
-
-	size_t size = 0;
-	int temp = 0;
-	int prevPos = ifs.tellg();
-
-	while (ifs >> temp) size++;
-
-	ifs.clear();
-	ifs.seekg(prevPos);
-	
-	return size;
-}
-
 void fillFile(const char* fileName)
 {
 	if (!fileName) return;
 
+	int N, num = 0;
+	std::cin >> N;
+
 	std::ofstream ofs(fileName);
 	if (!ofs.is_open()) return;
 
-	int N, element = 0;
-	std::cin >> N;
-
 	for (int i = 0; i < N; ++i)
 	{
-		std::cin >> element;
-		ofs << element << " ";
+		std::cin >> num;
+		ofs << num;
+		if (i != N - 1) ofs << " ";
 	}
 
 	ofs.close();
 }
 
-void fillRes(const char* fileName, int* arr,int size)
+size_t getCountOfNums(std::ifstream& ifs)
+{
+	if (!ifs.is_open()) return -1;
+	size_t count = 0;
+	size_t prevPos = ifs.tellg();
+
+	int temp = 0;
+	while (ifs >> temp) count++;
+
+	ifs.clear();
+	ifs.seekg(prevPos);
+
+	return count;
+}
+
+int* getArr(std::ifstream& ifs, size_t size)
+{
+	if (!ifs.is_open()) return nullptr;
+	size_t prevPos = ifs.tellg();
+
+
+	int* arr = new int[size];
+	for (int i = 0; i < size; ++i)
+	{
+		ifs >> arr[i];
+	}
+
+	ifs.clear();
+	ifs.seekg(prevPos);
+
+	return arr;
+}
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void bubbleSort(int* arr, size_t size, bool(*predicate)(int, int))
+{
+	for (int i = 0; i < size - 1; ++i)
+	{
+		for (int j = 0; j < size - i - 1; ++j)
+		{
+			if (predicate(arr[j], arr[j + 1])) swap(arr[j], arr[j + 1]);
+		}
+	}
+}
+
+void fillResFile(const char* fileName,int* arr, size_t size)
 {
 	if (!fileName) return;
 
@@ -49,62 +85,32 @@ void fillRes(const char* fileName, int* arr,int size)
 		if (i != size - 1) ofs << " ";
 	}
 
-
 	ofs.close();
 }
 
-int* getNums(std::ifstream& ifs, int size)
+void fillFiles(int* arr, size_t size)
 {
-	int* arr = new int[size];
-
-	for (int i = 0; i < size; ++i)
-	{
-		ifs >> arr[i];
-	}
-
-	return arr;
-}
-
-void swap(int& a, int& b)
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
-
-void bubbleSort(int* arr, int size, bool(*predicate)(int, int))
-{
-	if (!arr) return;
-
-	for (int i = 0; i < size - 1; ++i)
-	{
-		for (int j = 0; j < size - i - 1; ++j)
-		{
-			if (predicate(arr[j], arr[j + 1]))
-			{
-				swap(arr[j], arr[j + 1]);
-			}
-		}
-	}
-}
-
-int main()
-{
-	fillFile("result.txt");
-	std::ifstream ifs("result.txt");
-	if (!ifs.is_open()) return -1;
-
-	size_t size = getCountOfNums(ifs);
-	int* arr = getNums(ifs, size);
-
-	ifs.close();
 	bool (*ascending)(int, int) = [](int a, int b) -> bool {return a > b; };
 	bool (*descending)(int, int) = [](int a, int b) -> bool {return a < b; };
 
 	bubbleSort(arr, size, ascending);
-	fillRes("result1.txt", arr, size);
+	fillResFile("result1.txt", arr, size);
 	bubbleSort(arr, size, descending);
-	fillRes("result2.txt", arr, size);
+	fillResFile("result2.txt", arr, size);
+}
+
+int main()
+{
+	fillFile("test.txt");
+	std::ifstream ifs("test.txt");
+	if (!ifs.is_open()) return -1;
+
+	size_t size = getCountOfNums(ifs);
+	int* arr = getArr(ifs,size);
+
+	ifs.close();
+
+	fillFiles(arr, size);
 
 	delete[] arr;
 	return 0;
