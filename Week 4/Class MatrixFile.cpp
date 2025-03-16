30850f505b904827ed9a3b836c5f3fae1c7a330a
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2025-03-16 13:24:32 +0200

    Update Class MatrixFile.cpp

--- Week 4/Class MatrixFile.cpp
+++ Week 4/Class MatrixFile.cpp
@@ -6,6 +6,7 @@ namespace CONSTANTS
 	const int MAX_ROWS = 30;
 	const int MAX_COLS = 30;
 	const int DEFAULT_VALUE = 0;
+	const int MAX_FILENAME_LEN = 65;
 }
 
 class MatrixFile
@@ -14,6 +15,7 @@ private:
 	int matrix[CONSTANTS::MAX_ROWS][CONSTANTS::MAX_COLS]{};
 	int currentRows = 0;
 	int currentCols = 0;
+	char fileName[CONSTANTS::MAX_FILENAME_LEN] = "";
 
 	void setCurrentCols(std::ifstream& ifs)
 	{
@@ -81,6 +83,39 @@ private:
 		}
 	}
 
+	void strCopy(const char* source, char* dest)
+	{
+		int index = 0;
+		while (source[index] != '\0')
+		{
+			dest[index] = source[index];
+			index++;
+		}
+		dest[index] = '\0';
+	}
+
+	void saveMatrix(std::ofstream& ofs) const
+	{
+		if (!ofs.is_open()) return;
+
+		for (int i = 0; i < currentRows; ++i)
+		{
+			for (int j = 0; j < currentCols; ++j)
+			{
+				ofs << matrix[i][j];
+				if (j != currentCols - 1) ofs << " ";
+			}
+			if(i != currentRows - 1) ofs << "\n";
+		}
+	}
+
+	void saveRowsAndCols(std::ofstream& ofs) const
+	{
+		if (!ofs.is_open()) return;
+		ofs << currentRows << " ";
+		ofs << currentCols << "\n";
+	}
+
 public:
 	MatrixFile() : currentRows(CONSTANTS::DEFAULT_VALUE), currentCols(CONSTANTS::DEFAULT_VALUE) {};
 
@@ -91,11 +126,18 @@ public:
 
 		if (!ifs.is_open()) return;
 
+		setFileName(fileName);
 		setCurrentRows(ifs);
 		setCurrentCols(ifs);
 		setMatrix(ifs);
 		
 		ifs.close();
+	}
+
+	void setFileName(const char* fileName)
+	{
+		if (!fileName) return;
+		strCopy(fileName, this->fileName);
 	}
 
 	void print() const
@@ -115,14 +157,46 @@ public:
 		if (currentCols == currentCols) transposeNxN();
 		else transposeNxM();
 	}
+
+	void multiplyByNum(int num)
+	{
+		for (int i = 0; i < currentRows; ++i)
+		{
+			for (int j = 0; j < currentCols; ++j)
+			{
+				matrix[i][j] *= num;
+			}
+		}
+	}
+
+	void changeElement(int i, int j, int input)
+	{
+		if (i < 0 || i > currentRows) return;
+		if (j < 0 || j > currentCols) return;
+
+		matrix[i][j] = input;
+	}
+	
+	void saveMatrixToFile(const char* fileName) const
+	{
+		if (!fileName) return;
+		std::ofstream ofs(fileName);
+
+		if (!ofs.is_open()) return;
+		saveRowsAndCols(ofs);
+		saveMatrix(ofs);
+
+		ofs.close();
+	}
+
+	~MatrixFile()
+	{
+		saveMatrixToFile(this->fileName);
+	}
 };
 
 int main()
 {
-	MatrixFile matrix("test1.txt");
-	matrix.print();
-	matrix.transpose();
-	std::cout << std::endl;
-	matrix.print();
+	
 	return 0;
 }
