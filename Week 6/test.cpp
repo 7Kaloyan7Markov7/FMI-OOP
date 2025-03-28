cb540d5f75fede799e9d819b16e802abef6605d4
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2025-03-28 23:51:44 +0200

    Update test.cpp

--- Week 6/test.cpp
+++ Week 6/test.cpp
@@ -1,8 +1,47 @@
-#inclide <iostream>
+#include <iostream>
+
+class BitSet
+{
+private:
+	uint8_t* buckets = nullptr;
+	size_t maxCapacity = 8;
+	size_t biggestNum = 0;
+
+	void resize(size_t capacity)
+	{
+		uint8_t 
+	}
+	
+	void copyFrom(const BitSet& other)
+	{
+
+	}
+
+	void free()
+	{
+		delete[] buckets;
+		buckets = nullptr;
+		maxCapacity = 0;
+		biggestNum = 0;
+	}
+
+public:
+
+	BitSet() = default;
+	BitSet(int num)
+	{
+		addNumber(num);
+	}
+	
+	void addNumber(int num)
+	{
+
+	}
+};
+
 
 int main()
 {
-  int a = 0;
-  int b = 0;
-  return  0;
+
+	return 0;
 }
